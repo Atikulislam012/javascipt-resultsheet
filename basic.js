e9a253e7ef0855e940ed5975210fc0e446f0b077
Atikulislam012 <info.atikulislam015@gmail.com>
2023-09-11 21:35:34 +0600

    result sheet

---
+++ basic.js
@@ -0,0 +1,55 @@
+
+
+let Firstname = "Atikul";
+let Lastname = "Islam";
+
+let Roll = 482977;
+let Registration = 1502078546;
+
+let mark = 70;
+let Grade;
+let speach = " ";
+
+if (mark >= 90){
+    Grade = "Golden"
+}
+
+else if(mark >= 80){
+    Grade = "A+"
+}
+
+else if(mark >= 70){
+    Grade = "A"
+}
+
+else if(mark >= 60){
+    Grade = "A-"
+}
+
+else if(mark >= 50){
+    Grade = "B"
+}
+
+else if(mark >= 40){
+    Grade = "B+"
+}
+
+else if(mark >= 33){
+    Grade = "C"
+}
+
+else{
+    Grade = "F"
+}
+
+
+
+console.log(Firstname + speach + Lastname)
+
+console.log('Roll Number :' + speach + Roll)
+
+console.log('Registration Number :' + speach + Registration)
+
+console.log( 'You got the latter Grade')
+
+console.log(Grade)
